ca2bb8f186c3ffb049e7317560d29129b3e92b78
Indroic <indroic@outlook.com>
2026-04-08 06:53:28 -0400

    fix(aplication): firma erronea en el use case base

--- hexcore/application/use_cases/base.py
+++ hexcore/application/use_cases/base.py
@@ -3,13 +3,13 @@ from abc import ABC, abstractmethod
 from hexcore.application.dtos.base import DTO
 
 # Tipo del Input
-T = t.TypeVar("T", bound=t.Union[DTO, list[DTO]])
+T = t.TypeVar("T", bound=DTO)
 
 # Tipo del Output(o resultado)
-R = t.TypeVar("R", bound=t.Union[DTO, list[DTO]])
+R = t.TypeVar("R", bound=DTO)
 
 
 class UseCase(ABC, t.Generic[T, R]):
     @abstractmethod
-    async def execute(self, command: T) -> R:
+    async def execute(self, command: T | list[T]) -> R | list[R]:
         raise NotImplementedError("Subclasses must implement this method")
